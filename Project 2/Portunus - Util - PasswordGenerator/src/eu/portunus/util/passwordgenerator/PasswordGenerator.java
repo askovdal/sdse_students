7ee141ff28d490fd86f9d64eb5e299da5a2fd67a
Andreas Skovdal <andsk@itu.dk>
2021-10-31 14:00:44 +0100

    Implement PasswordGenerator

--- Project 2/Portunus - Util - PasswordGenerator/src/eu/portunus/util/passwordgenerator/PasswordGenerator.java
+++ Project 2/Portunus - Util - PasswordGenerator/src/eu/portunus/util/passwordgenerator/PasswordGenerator.java
@@ -1,20 +1,114 @@
 package eu.portunus.util.passwordgenerator;
 
-import java.util.Collection;
+import java.util.*;
 
 public class PasswordGenerator implements IPasswordGenerator {
+
+    Map<CharacterSet, String[]> characterSetMap;
+
+    public PasswordGenerator() {
+        characterSetMap = new HashMap<>();
+        characterSetMap.put(
+            CharacterSet.UPPER_CASE,
+            new String[] {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E",
+                "F",
+                "G",
+                "H",
+                "I",
+                "J",
+                "K",
+                "L",
+                "M",
+                "N",
+                "O",
+                "P",
+                "Q",
+                "R",
+                "S",
+                "T",
+                "U",
+                "V",
+                "W",
+                "X",
+                "Y",
+                "Z",
+            }
+        );
+        characterSetMap.put(
+            CharacterSet.LOWER_CASE,
+            new String[] {
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+                "h",
+                "i",
+                "j",
+                "k",
+                "l",
+                "m",
+                "n",
+                "o",
+                "p",
+                "q",
+                "r",
+                "s",
+                "t",
+                "u",
+                "v",
+                "w",
+                "x",
+                "y",
+                "z",
+            }
+        );
+        characterSetMap.put(
+            CharacterSet.DIGITS,
+            new String[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }
+        );
+        characterSetMap.put(CharacterSet.MINUS, new String[] { "-" });
+        characterSetMap.put(CharacterSet.UNDERSCORE, new String[] { "_" });
+        characterSetMap.put(CharacterSet.SPACE, new String[] { " " });
+    }
 
     @Override
     public String generatePassword(
         int length,
         Collection<CharacterSet> characterSets
     ) {
-        if (characterSets == null || characterSets.isEmpty()) {
+        if (characterSets == null || characterSets.isEmpty() || length < 1) {
             return "";
         }
 
-        //TODO: Implement the password generator.
+        // Remove duplicate character sets.
+        List<CharacterSet> characterSetsArray = new ArrayList<>(
+            new HashSet<>(characterSets)
+        );
 
-        return "";
+        Random random = new Random();
+        StringBuilder password = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            // Get a random character set.
+            CharacterSet characterSet = characterSetsArray.get(
+                random.nextInt(characterSetsArray.size())
+            );
+
+            // Get the characters from the random character set.
+            String[] characters = characterSetMap.get(characterSet);
+
+            // Get a random character and append it to the password.
+            String character = characters[random.nextInt(characters.length)];
+            password.append(character);
+        }
+
+        return password.toString();
     }
 }
--- Project 2/Portunus - Util - PasswordGenerator/src/eu/portunus/util/passwordgenerator/test/PasswordGeneratorTest.java
+++ Project 2/Portunus - Util - PasswordGenerator/src/eu/portunus/util/passwordgenerator/test/PasswordGeneratorTest.java
@@ -1,5 +1,60 @@
 package eu.portunus.util.passwordgenerator.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import eu.portunus.util.passwordgenerator.CharacterSet;
+import eu.portunus.util.passwordgenerator.PasswordGenerator;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PasswordGeneratorTest {
-    //TODO: Write tests for the password generator.
+
+    private PasswordGenerator passwordGenerator;
+    private List<CharacterSet> characterSets;
+
+    @BeforeEach
+    void setUp() {
+        passwordGenerator = new PasswordGenerator();
+        characterSets = new ArrayList<>();
+    }
+
+    @Test
+    void ReturnEmptyString_When_ZeroPasswordLength() {
+        characterSets.add(CharacterSet.UPPER_CASE);
+        String password = passwordGenerator.generatePassword(0, characterSets);
+        assertEquals("", password);
+    }
+
+    @Test
+    void ReturnEmptyString_When_NegativePasswordLength() {
+        characterSets.add(CharacterSet.UPPER_CASE);
+        String password = passwordGenerator.generatePassword(
+            -42,
+            characterSets
+        );
+        assertEquals("", password);
+    }
+
+    @Test
+    void ReturnEmptyString_When_NullCharacterSet() {
+        String password = passwordGenerator.generatePassword(42, null);
+        assertEquals("", password);
+    }
+
+    @Test
+    void ReturnEmptyString_When_EmptyCharacterSet() {
+        String password = passwordGenerator.generatePassword(42, characterSets);
+        assertEquals("", password);
+    }
+
+    @Test
+    void PasswordIsNChars_When_LengthIsN() {
+        characterSets.add(CharacterSet.UPPER_CASE);
+        characterSets.add(CharacterSet.LOWER_CASE);
+        characterSets.add(CharacterSet.DIGITS);
+        String password = passwordGenerator.generatePassword(42, characterSets);
+        assertEquals(42, password.length());
+    }
 }
